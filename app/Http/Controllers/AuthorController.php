e8ed3ee955172097b4d3e4b04e0d8c69d49689b4
Vioscar Rivero <szchmausser@gmail.com>
2021-04-08 14:44:32 -0400

    Seccion 2 / Leccion 10 y 11: Creado controlador y rutas para Author.

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    public function index(){
+        $authors = Author::all();
+        return response()->json(['data' => ['authors' => $authors]]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource('/authors', AuthorController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
